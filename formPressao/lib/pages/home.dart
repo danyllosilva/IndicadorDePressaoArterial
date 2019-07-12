c198c121e7677e1661e936632f17019911a22e62
Luan Carlos <luancmjesus@gmail.com>
2019-07-12 20:59:37 -0300

    Adição de CarouselSlider na homePage

--- formPressao/lib/pages/home.dart
+++ formPressao/lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import 'sobre.dart';
 import 'tiposPressao.dart';
@@ -12,22 +14,48 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  var url = 'https://st4.depositphotos.com/14846838/22137/v/1600/depositphotos_221379676-stock-illustration-person-avatar-stethoscope-symbolizing-part.jpg';
+  List<Widget> listaTela = new List();
+  CarouselSlider instancia;
+
+  @override
+  void initState(){
+    super.initState();
+
+    listaTela.add(info1());
+    listaTela.add(info2());
+    listaTela.add(info3());
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    instancia = CarouselSlider(
+      autoPlay: true,
+      autoPlayAnimationDuration: Duration(seconds: 3),
+      items: listaTela.map(
+        (it){
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            decoration: BoxDecoration(
+              color: Colors.black12
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                it
+              ],
+            ),
+          );
+        }
+      ).toList(),
+      height: MediaQuery.of(context).size.height-150,
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Text('Avaliador de pressão'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Página Inicial')
-          ],
-        )
       ),
       drawer: Drawer(
         child: ListView(
@@ -58,7 +86,42 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
-      )
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            instancia 
+          ],
+        )
+      ),
     );
   }
+}
+
+info1(){
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+        Text('Informações do App',)
+    ],
+  );
+}
+
+info2(){
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+        Text('Informações do App - 2')
+    ],
+  );
+}
+
+info3(){
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+        Text('Informações do App - 3')
+    ],
+  );
 }

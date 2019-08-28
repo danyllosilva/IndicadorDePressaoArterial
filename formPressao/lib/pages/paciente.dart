29ae38a26dc6d3d198164ab33d4c3407891108ec
DanylloCarlos <danyllo.rsilva@gmail.com>
2019-08-28 19:17:40 -0300

    Ajustes de indentação

--- formPressao/lib/pages/paciente.dart
+++ formPressao/lib/pages/paciente.dart
@@ -11,21 +11,6 @@ class Paciente {
  
   Paciente(this.nome, this.perfil, this.sexo, this.cpf, this.idade, this.dtNascimento, this.endereco, this.telefone);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['nome'] = this.nome;
-    data['perfil'] = this.cpf;
-    data['sexo'] = this.sexo;
-    data['cpf'] = this.cpf;
-    data['idade'] = this.idade;
-    data['dtNascimento'] = this.dtNascimento;
-    data['endereco'] = this.endereco;
-    data['telefone'] = this.telefone;
-    return data;
-  }
-
-
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
       'id': this.id,
